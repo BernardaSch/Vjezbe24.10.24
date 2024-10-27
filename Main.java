9758ba7a7e54c709bef64976155be802f6212eaa
Bernarda <bernarda.schwalba@gmail.com>
2024-10-27 20:57:51 +0100

    Drugi zadatak

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
+
 /*
-Statistika riječi
-Napravi program koji učitava rečenicu i ispisuje:
-Sve riječi duže od 5 slova
-Najdužu riječ
-Prosječnu duljinu riječi
+2. Telefonski imenik:
+Napravi program koji simulira telefonski imenik koristeći dvije paralelne liste:
+Lista u koju se spremaju imena, i lista u kojoj se sprema brojevi
+Kod prvog odabira korisnik unosi ime i broj koji se spremaju u svoje liste
+Korisnik ima mogucnost odabira, te za svaki odabir treba omoguciti odredenu funkcionalnost sa slike.
 */
 
 import java.util.ArrayList;
@@ -11,40 +12,85 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static List<String> imena = new ArrayList<>();
+    static List<String> brojevi = new ArrayList<>();
+    static int odabir;
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        List<String> rijeciDuzeOdPetSlova =  new ArrayList<>();
+        System.out.println("\n***** Odaberite opciju: *****");
 
-        int brojSlova = 0;
-        int ukupnaDuljinaSvihRijeciURecenici = 0;
-        String najduzaRijec = "";
-        double prosjek = 0;
-        String recenica = "";
+        do {
+            System.out.println("\n1. Dodaj kontakt ");
+            System.out.println("2. Pretraži po imenu ");
+            System.out.println("3. Pretraži po broju ");
+            System.out.println("4. Izbriši sve ");
+            System.out.println("\n");
 
-        System.out.println("Unesite rečenicu: ");
-        recenica = scanner.nextLine();
+            odabir = scanner.nextInt();
+            scanner.nextLine();
 
-        String[] rijeci = recenica.split("\\s+");
-
-        for (String rijec : rijeci){
-            brojSlova=rijec.length();
-            ukupnaDuljinaSvihRijeciURecenici = ukupnaDuljinaSvihRijeciURecenici + brojSlova;
-
-            if (brojSlova>5) {
-                rijeciDuzeOdPetSlova.add(rijec);
+            switch (odabir){
+                case 1:
+                    dodavanjeKontakta();
+                    break;
+                case 2:
+                    pretrazivanjePoImenu();
+                    break;
+                case 3:
+                    pretrazivanjePoBroju();
+                    break;
+                case 4:
+                    izbrisiSve();
+                    break;
+                default:
+                    System.out.println("Krivi odabir. Pokušajte ponovno.");
+                    break;
             }
+        }while(odabir == 1 || odabir == 2 || odabir == 3 || odabir == 4);
+    }
 
-            if (brojSlova > najduzaRijec.length()){
-                najduzaRijec = rijec;
-            }
+    private static void dodavanjeKontakta(){
+        System.out.println("Unesite ime i prezime: ");
+        String imeIPrezime = scanner.nextLine();
 
+        System.out.println("Unesite telefonski broj: ");
+        String telBroj = scanner.nextLine();
+
+        imena.add(imeIPrezime);
+        brojevi.add(telBroj);
+        System.out.println("Kontakt je unesen.");
+    }
+
+    private static void pretrazivanjePoImenu(){
+        System.out.println("Unesite ime ili prezime ");
+        String imeZaPretragu = scanner.nextLine();
+
+        int i = imena.indexOf(imeZaPretragu);
+        if (i != -1) {
+            System.out.println("Pronađeni podaci: " + imena.get(i) + " " + brojevi.get(i));
+        } else {
+            System.out.println("Podaci nisu pronađeni.");
         }
-        prosjek =  (double)ukupnaDuljinaSvihRijeciURecenici / rijeci.length;
+    }
 
-        System.out.println("Riječi duže od pet slova: " + rijeciDuzeOdPetSlova);
-        System.out.println("Najduža riječ: " + najduzaRijec);
-        System.out.println("Prosjek duljine svih riječi: " + prosjek);
+    private static void pretrazivanjePoBroju() {
+        System.out.print("Unesite broj za pretragu: ");
+        String brojZaPretragu = scanner.nextLine();
 
+        int i = brojevi.indexOf(brojZaPretragu);
+        if (i != -1) {
+            System.out.println("Pronađeni kontakt: " + imena.get(i) + "  " + brojevi.get(i));
+        } else {
+            System.out.println("Podaci nisu pronađeni.");
+        }
+    }
+
+    private static void izbrisiSve() {
+        imena.clear();
+        brojevi.clear();
+        System.out.println("Obrisano.");
     }
 }
